515199b87769978165ce46829b083c43d3eec5eb
Fatima Zahra ZACRAOUI <FatimaZahra.ZACRAOUI.next@ag2rlamondiale.fr>
2020-08-04 15:15:00 +0200

    commit refactoring changes

--- sm-shop/src/main/java/com/salesmanager/shop/admin/controller/user/UserController.java
+++ sm-shop/src/main/java/com/salesmanager/shop/admin/controller/user/UserController.java
@@ -1,12 +1,6 @@
 package com.salesmanager.shop.admin.controller.user;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import javax.inject.Inject;
 import javax.inject.Named;
